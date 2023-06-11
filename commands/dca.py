8ec60cbc1142410a0dd16ef49c582fde15b9f5e9
AccursedGalaxy <robinbohrer7@gmail.com>
2023-06-11 14:09:21 +0200

    switched to CMC API for data

--- commands/dca.py
+++ commands/dca.py
@@ -1,22 +1,41 @@
-import ccxt
+import requests
 import logging
-from setup import exchanges, coins, near_percentage
+import asyncio
+import traceback
+from setup import coins, near_percentage
 from utils import loading_message
+
+from config.settings import DISCORD_TOKEN, CMC_API_KEY
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('discord').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-def fetch_price(exchange, coin):
-    """Fetch the price for a coin from an exchange."""
+def fetch_prices(coins):
+    """Fetch the prices for multiple coins from CoinMarketCap."""
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+    headers = {
+        "Accepts": "application/json",
+        "X-CMC_PRO_API_KEY": CMC_API_KEY,
+    }
+    parameters = {
+        "symbol": ','.join(coins),
+        "convert": "USD"
+    }
     try:
-        return exchange.fetch_ticker(coin)['last']
+        response = requests.get(url, headers=headers, params=parameters)
+        data = response.json()['data']
+        prices = {coin: float(data[coin]['quote']['USD']['price']) for coin in coins}
+        logger.info(f"Prices: {prices}")
+        return prices
     except Exception as e:
-        logger.error(f"Could not fetch price for {coin} from {exchange.name}: {e}")
+        logger.error(f"Could not fetch prices: {e}")
         return None
+
 
 def check_levels(coin, levels, price):
     """Check if the price is near a DCA or profit level."""
+    logger.info(f"Checking levels for {coin} at {price}")
     for level_type, level_values in levels.items():
         for level in level_values:
             if (1 - near_percentage) * level <= price <= (1 + near_percentage) * level:
@@ -25,26 +44,26 @@ def check_levels(coin, levels, price):
 
 async def generate_dca_response():
     """Generate the DCA response."""
-    exchange_objects = [getattr(ccxt, name)() for name in exchanges]
     response = "DCA Levels:\n"
+    logger.info("Generating DCA response")
 
     try:
-        for coin, levels in coins.items():
-            found_price = False
-            for exchange in exchange_objects:
-                price = fetch_price(exchange, coin)
+        prices = fetch_prices(coins.keys())
+        if prices is not None:
+            for coin, levels in coins.items():
+                price = prices.get(coin)
                 if price is not None:
-                    found_price = True
                     result = check_levels(coin, levels, price)
                     if result:
                         response += f"{result} - Current Price: {price}\n"
-            if not found_price:
-                response += f"Could not fetch price for {coin} from any exchange.\n"
+                else:
+                    response += f"Could not fetch price for {coin}.\n"
 
         return response
     except Exception as e:
         logger.error(f"Could not generate DCA response: {e}")
         return None
+
 
 @loading_message
 async def execute(message, args):
--- config/settings.py
+++ config/settings.py
@@ -9,3 +9,4 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Load Discord bot token
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+CMC_API_KEY = os.getenv("CMC_API_KEY")
