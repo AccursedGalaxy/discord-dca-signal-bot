83b7754647da25978baca7785cd162a6e02cf774
AccursedGalaxy <robinbohrer7@gmail.com>
2023-06-11 04:22:32 +0200

    better code structure

--- bot.py
+++ bot.py
@@ -1,63 +1,25 @@
-import ccxt
+import os
+import importlib
 import logging
 import discord
-import asyncio
-
-from config.settings import DISCORD_TOKEN, CHANNEL_ID
-from setup import exchanges, coins, near_percentage
-
-import responses
+from config.settings import DISCORD_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('discord').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-def fetch_price(exchange, coin):
-    """Fetch the price for a coin from an exchange."""
+commands = {}
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        commands[filename[:-3]] = importlib.import_module(f'commands.{filename[:-3]}')
+
+async def send_message(message, user_message):
     try:
-        return exchange.fetch_ticker(coin)['last']
-    except Exception as e:
-        logger.error(f"Could not fetch price for {coin} from {exchange.name}: {e}")
-        return None
-
-def check_levels(coin, levels, price):
-    """Check if the price is near a DCA or profit level."""
-    for level_type, level_values in levels.items():
-        for level in level_values:
-            if (1 - near_percentage) * level <= price <= (1 + near_percentage) * level:
-                return f"{coin} Hit a {level_type} Level: {level}"
-    return None
-
-async def generate_dca_response():
-    """Generate the DCA response."""
-    exchange_objects = [getattr(ccxt, name)() for name in exchanges]
-    response = "DCA Levels:\n"
-
-    try:
-        for coin, levels in coins.items():
-            found_price = False
-            for exchange in exchange_objects:
-                price = fetch_price(exchange, coin)
-                if price is not None:
-                    found_price = True
-                    result = check_levels(coin, levels, price)
-                    if result:
-                        response += f"{result} - Current Price: {price}\n"
-            if not found_price:
-                response += f"Could not fetch price for {coin} from any exchange.\n"
-
-        return response
-    except Exception as e:
-        logger.error(f"Could not generate DCA response: {e}")
-        return None
-
-async def send_message(message, user_message, is_private):
-    try:
-        if user_message == "!dca":
-            response = await generate_dca_response()
-        else:
-            response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        if user_message.startswith('!'):
+            command = user_message[1:].lower()
+            if command in commands:
+                await commands[command].execute(message, [])
     except Exception as e:
         logging.error(e)
 
@@ -84,11 +46,9 @@ def run_discord_bot():
         print(f'{username}: {user_message} ({channel})')
 
         if user_message.startswith('!'):
-            command = user_message[1:].lower()
-            if command == "help" or command == "dca":
-                await send_message(message, user_message, False)
+            await send_message(message, user_message)
         else:
-            await send_message(message, user_message, False)
+            await send_message(message, user_message)
 
     client.run(TOKEN)
 
---
+++ commands/dca.py
@@ -0,0 +1,51 @@
+import ccxt
+import logging
+from setup import exchanges, coins, near_percentage
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('discord').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+def fetch_price(exchange, coin):
+    """Fetch the price for a coin from an exchange."""
+    try:
+        return exchange.fetch_ticker(coin)['last']
+    except Exception as e:
+        logger.error(f"Could not fetch price for {coin} from {exchange.name}: {e}")
+        return None
+
+def check_levels(coin, levels, price):
+    """Check if the price is near a DCA or profit level."""
+    for level_type, level_values in levels.items():
+        for level in level_values:
+            if (1 - near_percentage) * level <= price <= (1 + near_percentage) * level:
+                return f"{coin} Hit a {level_type} Level: {level}"
+    return None
+
+async def generate_dca_response():
+    """Generate the DCA response."""
+    exchange_objects = [getattr(ccxt, name)() for name in exchanges]
+    response = "DCA Levels:\n"
+
+    try:
+        for coin, levels in coins.items():
+            found_price = False
+            for exchange in exchange_objects:
+                price = fetch_price(exchange, coin)
+                if price is not None:
+                    found_price = True
+                    result = check_levels(coin, levels, price)
+                    if result:
+                        response += f"{result} - Current Price: {price}\n"
+            if not found_price:
+                response += f"Could not fetch price for {coin} from any exchange.\n"
+
+        return response
+    except Exception as e:
+        logger.error(f"Could not generate DCA response: {e}")
+        return None
+
+async def execute(message, args):
+    response = await generate_dca_response()
+    await message.channel.send(response)
+
